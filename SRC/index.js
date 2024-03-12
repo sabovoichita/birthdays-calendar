156fa4d087f729962ec4967b67dbff9e0d526ec0
Voichita Maria <sabo_voichita@yahoo.com>
2024-03-12 19:08:12 +0100

    render Birthdays

--- SRC/index.js
+++ SRC/index.js
@@ -1,9 +1,23 @@
 import "./style.css";
 
+function getBirthdayAsHTML(birthday) {
+  return `<tr>
+  <td>${birthday.name}</td>
+  <td>${birthday.contact}</td>
+  <td>${birthday.age}</td>
+  <td>${birthday.DOB}</td>
+</tr>`;
+}
+
+function renderBirthdays(birthdays) {
+  const birthdayHTML = birthdays.map(getBirthdayAsHTML);
+  document.querySelector("#birthdayTable tbody").innerHTML = birthdayHTML.join("");
+}
+
 function loadBirthdays() {
   fetch("birthdays.json").then(r =>
     r.json().then(birthdays => {
-      console.warn("birthdays?", birthdays);
+      renderBirthdays(birthdays);
     })
   );
 }
