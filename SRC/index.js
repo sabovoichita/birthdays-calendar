8293319929b454f9087f13e971858963c1fe3e01
Voichita Maria <sabo_voichita@yahoo.com>
2024-03-14 12:51:21 +0100

    wait for request to be done before we reload(createBirthdayRequest, onSubmit, initEvents,$ functions )

--- SRC/index.js
+++ SRC/index.js
@@ -1,5 +1,19 @@
 import "./style.css";
 
+function $(selector) {
+  return document.querySelector(selector);
+}
+
+function createBirthdayRequest(birthday) {
+  return fetch("http://localhost:3000/birthdays-json/create", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json"
+    },
+    body: JSON.stringify(birthday)
+  }).then(r => r.json());
+}
+
 function getBirthdayAsHTML(birthday) {
   return `<tr>
   <td>${birthday.name}</td>
@@ -13,7 +27,7 @@ function getBirthdayAsHTML(birthday) {
 
 function renderBirthdays(birthdays) {
   const birthdayHTML = birthdays.map(getBirthdayAsHTML);
-  document.querySelector("#birthdayTable tbody").innerHTML = birthdayHTML.join("");
+  $("#birthdayTable tbody").innerHTML = birthdayHTML.join("");
 }
 
 function loadBirthdays() {
@@ -28,5 +42,33 @@ function loadBirthdays() {
     })
   );
 }
+function onSubmit(e) {
+  // console.warn("submit", e);
+  e.preventDefault();
 
+  const contact = $("input[name=contact]").value;
+  const age = $("#age").value;
+  const url = $("#url").value;
+  const DOB = $("#DOB").value;
+
+  const birthday = {
+    name: $("input[name=name]").value,
+    contact: contact,
+    age: age,
+    url,
+    DOB: DOB
+  };
+
+  createBirthdayRequest(birthday).then(status => {
+    // console.warn("status", status);
+    if (status.success) {
+      window.location.reload();
+    }
+  });
+}
+
+function initEvents() {
+  $("#birthdaysForm").addEventListener("submit", onSubmit);
+}
+initEvents();
 loadBirthdays();
